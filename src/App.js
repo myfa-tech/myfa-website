ae0aff00f49e7bd3e8979729022c860bc30d9e61
Florian Adonis <tanohzana@gmail.com>
2019-08-18 12:36:54 +0200

    section2 OK

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react'
-import './App.css'
+import './App.scss'
 import Section1 from './components/Section1'
 import Navbar from './components/common/Navbar'
+import Section2 from './components/Section2'
 
 function App() {
   return (
     <>
       <Navbar />
       <Section1 />
+      <Section2 />
     </>
   )
 }
---
+++ src/components/Section2/Section2.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+import { IoIosCloudOutline } from 'react-icons/io'
+
+import watchSrc from '../../assets/images/watch.png'
+import './Section2.scss'
+
+const Section2 = () => (
+  <Container className='section'>
+    <Row>
+      <div className='heading'>
+        <h2>Our Core Features</h2>
+      </div>
+      <Col md={4}>
+        <div className='feature-box'>
+          <IoIosCloudOutline className='small-icon' />
+          <h4>Smooth Touch</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, inventore?</p>
+        </div>
+        <div className='feature-box'>
+          <IoIosCloudOutline className='small-icon' />
+          <h4>Smooth Touch</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, inventore?</p>
+        </div>
+        <div className='feature-box'>
+          <IoIosCloudOutline className='small-icon' />
+          <h4>Smooth Touch</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, inventore?</p>
+        </div>
+      </Col>
+      <Col md={4} className='image-container'>
+        <img src={watchSrc} alt='watch' />
+      </Col>
+      <Col md={4}>
+        <div className='feature-box'>
+          <IoIosCloudOutline className='small-icon' />
+          <h4>Smooth Touch</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, inventore?</p>
+        </div>
+        <div className='feature-box'>
+          <IoIosCloudOutline className='small-icon' />
+          <h4>Smooth Touch</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, inventore?</p>
+        </div>
+        <div className='feature-box'>
+          <IoIosCloudOutline className='small-icon' />
+          <h4>Smooth Touch</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, inventore?</p>
+        </div>
+      </Col>
+    </Row>
+  </Container>
+)
+
+export default Section2
