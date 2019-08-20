6fe234f96bf93a55d44dc2eccffd7545ad6ed424
Florian Adonis <tanohzana@gmail.com>
2019-08-20 22:26:56 +0200

    complete article - TODO: add imgs

--- src/components/Articles/Article1/Article1.js
+++ src/components/Articles/Article1/Article1.js
@@ -28,6 +28,83 @@ const Article1 = () => (
           Vous comprendrez donc d’où le chanteur Stromae tient son nom… <br />
           Le y ? juste pour le style !
         </p>
+
+        <p>
+          Si comme moi, vous êtes d’origine Africaine, ou même natif du continent
+          avec de la famille restée là-bas, vous allez reconnaître les situations suivantes :
+          « Pardon, mon téléphone m’a lâché, tu peux m’envoyer l’argent ? »,
+          « J’ai besoin de 35 000 FCFA pour m’inscrire au foot, tu peux m’aider ? »,
+          « J’ai besoin d’un laptop » … et j’en passe !
+        </p>
+
+        <p>
+          En effet, en tant que membre de la diaspora africaine, vous avez la mission de prendre en
+          charge une partie des dépenses de vos proches restés au pays. Ce n’est pas directement mon cas,
+          mais c’est bel et bien celui de mes parents.
+        </p>
+
+        <p>
+          Attendez, j’allais oublier… laissez-moi me présenter !
+        </p>
+
+        <Row>
+          <Col sm={3}>
+
+          </Col>
+          <Col>
+            <p>
+              Je m’appelle Doris SOMON, j’ai vu le jour au cours du mois de juillet 1995,
+              et depuis… j’improvise. Aujourd’hui, je vis un vrai tournant : la création d’entreprise.
+            </p>
+
+            <p>
+              Ce sourire ? J’essaye de le garder en toutes circonstances, même quand après six heures de
+              vol pour aller dans mon cher pays d’origine qu’est le Cameroun, la première question que
+              l’on me pose est « Qu’est-ce que tu m’as ramené ? ».
+            </p>
+          </Col>
+        </Row>
+
+        <p>
+          Cette année, c’est décidé, l’envoi d’argent c’est terminé. Vous penserez que cette décision est brutale…
+          loin de là, elle est très bien pensée. <br />
+          Laissez-moi vous expliquer : <br />
+          L’envoi d’argent ne me permet pas d’être sûre que le frigo de ma grand-mère est plein.
+        </p>
+
+        <p>
+          Pour avoir cette certitude, j’ai décidé (en étant très bien accompagnée :
+          Florian Adonis - <strong>CTO</strong>; Morgane CHASSON - <strong>Graphiste</strong>;
+          Marc Blanchard - <strong>Business Angel</strong>) de créer Myfa :
+          l’application pour vos proches, au loin.
+        </p>
+
+        <Row>
+          <Col sm={3}>
+
+          </Col>
+          <Col>
+            <p>
+              Grâce à cette application, ce n’est plus un don financier qui est réalisé,
+              mais plutôt la composition d’un panier alimentaire : d’ici, je fais des courses pour ma grand-mère,
+              elle n’a plus qu’à se rendre dans le magasin choisi pour récupérer ce panier (entre nous,
+              on sait qu’elle enverra un enfant aller le chercher).
+            </p>
+          </Col>
+        </Row>
+
+        <p>
+          De cette manière, j’ai un certain contrôle sur l’argent que j’envoie au Cameroun. <br />
+          Imaginons la situation suivante : tous les mois, j’envoie 100 euros à ma grand-mère.
+          Jusqu’à présent, impossible de vérifier que cet argent est utilisé à bon escient.
+          Grâce à Myfa, sur les 100 euros je peux faire 40-50 euros de courses,
+          lui permettant de se faire plaisir avec le reste.
+        </p>
+
+        <p className='quote'>
+          Que pouvez-vous faire pour promouvoir la paix dans le monde ? Rentrer chez vous et aimer votre famille !
+          Mère Teresa (1910-1997)
+        </p>
       </Col>
     </Row>
   </Container>
