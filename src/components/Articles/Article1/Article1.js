f50122bfb6d56702a5eccf53361e1ee2e4790ef4
Florian Adonis <tanohzana@gmail.com>
2019-08-20 09:11:37 +0200

    add author in article

--- src/components/Articles/Article1/Article1.js
+++ src/components/Articles/Article1/Article1.js
@@ -1,21 +1,36 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Container, Col, Row } from 'react-bootstrap'
+import moment from 'moment'
 
-const Article1 = () => {
-  return (
-    <Container>
-      <h1>Myfa, une application pensée pour la mif</h1>
+import '../Article.scss'
 
-      <p>
-        Hein ? Quoi ? Que veut dire “Myfa” ? <br />
-        Cela veut dire “famille” en verlan.
-        Le verlan étant une forme d'argot français qui consiste en
-        l'inversion des syllabes d'un mot.
-        Vous comprendrez donc d’où le chanteur Stromae tient son nom… <br />
-        Le y ? juste pour le style !
-      </p>
-    </Container>
-  )
-}
+import dorisSrc from '../../../assets/images/doris.png'
+
+const Article1 = () => (
+  <Container className='article-container'>
+    <Row>
+      <Col md={{ span: 8, offset: 2 }}>
+        <h1>Myfa, une application pensée pour la mif</h1>
+
+        <div className='author'>
+          <img src={dorisSrc} />
+          <div>
+            <span>Doris Somon</span>
+            <span className='date'>{moment().format('D MMM YYYY')}</span>
+          </div>
+        </div>
+
+        <p>
+          Hein ? Quoi ? Que veut dire “Myfa” ? <br />
+          Cela veut dire “famille” en verlan.
+          Le verlan étant une forme d'argot français qui consiste en
+          l'inversion des syllabes d'un mot.
+          Vous comprendrez donc d’où le chanteur Stromae tient son nom… <br />
+          Le y ? juste pour le style !
+        </p>
+      </Col>
+    </Row>
+  </Container>
+)
 
 export default Article1
