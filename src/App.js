45a1e8819c57293804a64f8427102fe1a22f0e85
Florian Adonis <tanohzana@gmail.com>
2019-08-20 08:10:21 +0200

    WIP - link to article 1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import { Switch, Route } from 'react-router'
+import { BrowserRouter } from 'react-router-dom'
+
 import './App.scss'
 
 import Navbar from './components/common/Navbar'
@@ -8,16 +11,28 @@ import Partners from './components/Partners'
 import News from './components/News'
 import Newsletter from './components/Newsletter'
 import Footer from './components/common/Footer'
+import Article1 from './components/Articles/Article1'
+
+const Main = () => (
+  <>
+    <Home />
+    <Features />
+    <Partners />
+    <News />
+    <Newsletter />
+  </>
+)
 
 function App() {
   return (
     <>
       <Navbar />
-      <Home />
-      <Features />
-      <Partners />
-      <News />
-      <Newsletter />
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Main}/>
+            <Route path="/articles/1" component={Article1}/>
+          </Switch>
+        </BrowserRouter>
       <Footer />
     </>
   )
---
+++ src/components/Articles/Article1/Article1.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+const Article1 = () => {
+  return (
+    <Container>
+      <h1>Myfa, une application pensée pour la mif</h1>
+
+      <p>
+        Hein ? Quoi ? Que veut dire “Myfa” ? <br />
+        Cela veut dire “famille” en verlan.
+        Le verlan étant une forme d'argot français qui consiste en
+        l'inversion des syllabes d'un mot.
+        Vous comprendrez donc d’où le chanteur Stromae tient son nom… <br />
+        Le y ? juste pour le style !
+      </p>
+    </Container>
+  )
+}
+
+export default Article1
