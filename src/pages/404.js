0533be1d1207ad7698c6d6db4e35953daaa9183a
Florian Adonis <tanohzana@gmail.com>
2019-10-11 17:55:10 +0000

    change 404 text

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,8 +5,8 @@ import SEO from "../components/seo"
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
+    <SEO title="404: Introuvable" />
+    <h1>PAGE INTROUVABLE</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
 )
