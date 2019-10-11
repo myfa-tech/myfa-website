8d0fdef1fd4caee20932be9cdb93154420981964
Florian Adonis <tanohzana@gmail.com>
2019-10-11 12:46:47 +0000

    add bot visual
    * add bot visual
    
    * add validators and saving to mailchimp

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,17 +1,68 @@
 import React from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
-import visuAppSrc from '../../images/visu_app_1.png'
+import ChatBot from 'react-simple-chatbot'
+import { ThemeProvider } from 'styled-components'
 
 import './Home.scss'
+import { isEmptyString } from 'is-what'
 
-const Home = () => {
+const steps = [
+  {
+    id: '1',
+    message: 'Salut ! Nous recherchons nos premiers utilisateurs testeurs de notre nouvelle solution innovante ! Intéressé(e) ? 😉 Comment tu t\'appelles ?',
+    trigger: '2',
+  },
+  {
+    id: '2',
+    user: true,
+    validator: (value) => {
+      if (!value || value === '') {
+        return 'prénom invalide';
+      }
+      return true;
+    },
+    trigger: '3',
+  },
+  {
+    id: '3',
+    message: 'Enchanté {previousValue}. J\'ai simplement besoin de ton email. Myfa te contactera très prochainement 😉',
+    trigger: '4',
+  },
+  {
+    id: '4',
+    user: true,
+    validator: (value) => {
+      if (!value.includes('@')) {
+        return 'adresse invalide';
+      }
+      return true;
+    },
+    trigger: '5',
+  },
+  {
+    id: '5',
+    message: 'Merci ! Restons en contact ✌🏽🌍',
+    end: true,
+  },
+]
+
+const theme = {
+  background: '#f5f8fb',
+  fontFamily: 'Helvetica Neue',
+  headerBgColor: '#f1d866',
+  headerFontColor: '#fff',
+  headerFontSize: '15px',
+  botBubbleColor: '#f1d866',
+  botFontColor: '#4a4a4a',
+  userBubbleColor: '#fff',
+  userFontColor: '#4a4a4a',
+}
+
+const Home = ({ handleEndChatBot }) => {
   return (
     <section id='home'>
       <Container className='section-1'>
         <Row>
-          <Col md={6} className='image-container'>
-            <img src={visuAppSrc} alt='iphone with myfa logo' />
-          </Col>
           <Col md={6}>
             <h1>Myfa, pour vos proches au loin</h1>
             <p>
@@ -25,6 +76,16 @@ const Home = () => {
             <span className='btn-main'>
               Bientôt disponible
             </span>
+          </Col>
+          <Col md={6} className='image-container'>
+            <ThemeProvider theme={theme}>
+              <ChatBot
+                handleEnd={handleEndChatBot}
+                headerTitle='Parlez-nous'
+                steps={steps}
+                placeholder='Entrez votre message ...'
+              />
+            </ThemeProvider>
           </Col>
         </Row>
       </Container>
