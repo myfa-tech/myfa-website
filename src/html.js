ae7fbf9887ffa32b5b8a42db698e67f0a90ac6c9
Florian Adonis <tanohzana@gmail.com>
2020-01-23 14:59:24 +0100

    insert crisp and remove hotjar

--- src/html.js
+++ src/html.js
@@ -28,15 +28,14 @@ export default function HTML(props) {
         <div id="fb-root"></div>
         <script dangerouslySetInnerHTML={{
           __html: `
-          (function(h,o,t,j,a,r){
-              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-              h._hjSettings={hjid:1476160,hjsv:6};
-              a=o.getElementsByTagName('head')[0];
-              r=o.createElement('script');r.async=1;
-              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-              a.appendChild(r);
-          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`
-        }} />
+            window.$crisp=[];
+            window.CRISP_WEBSITE_ID="f2462632-645d-4f57-b1b7-f7e5ac9c96ae";
+            d=document;
+            s=d.createElement("script");
+            s.src="https://client.crisp.chat/l.js";
+            s.async=1;
+            d.getElementsByTagName("head")[0].appendChild(s);
+          `}} />
       </body>
     </html>
   )
