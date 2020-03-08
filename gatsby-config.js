7bd25f8bc3cfab3988c3b37065233f7e19890f1b
Florian Adonis <tanohzana@gmail.com>
2020-03-08 15:11:47 +0000

    add weights to josefin font in gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
         fonts: [
-          `Josefin sans`,
+          `Josefin sans\:300,400,500,600,700`,
           `source sans pro\:300,400,400i,700`
         ],
         display: 'swap'
