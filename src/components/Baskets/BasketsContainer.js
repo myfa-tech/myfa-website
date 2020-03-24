39b1f1b905c362f30f453fc3e8ca0be26e5464f6
Florian Adonis <tanohzana@gmail.com>
2020-03-24 14:56:47 +0000

    make basketsContainer functional component

--- src/components/Baskets/BasketsContainer.js
+++ src/components/Baskets/BasketsContainer.js
@@ -1,26 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Baskets from './Baskets';
 import basketsInfos from '../../assets/baskets';
 import { customBasketDetails } from '../../assets/customBasket';
 import EventEmitter from '../../services/EventEmitter';
+import useTranslate from '../../hooks/useTranslate';
 
-class BasketsContainer extends React.Component {
-	state = {
-		basketForCart: null,
-		showCartModal: false,
+const BasketsContainer = () => {
+	const [basketForCart, setBasketForCart] = useState(null);
+	const [showCartModal, setShowCartModal] = useState(false);
+	const [t, locale] = useTranslate();
+
+	const eventEmitter = new EventEmitter();
+
+	const toggleCartModal = () => {
+		if (!!showCartModal) {
+			setBasketForCart(null);
+		}
+
+		setShowCartModal(!showCartModal);
 	};
 
-	eventEmitter = new EventEmitter();
-
-	toggleCartModal = () => {
-		const state = this.state.showCartModal ?
-			{ showCartModal: !this.state.showCartModal, basketForCart: null } :
-			{ showCartModal: !this.state.showCartModal };
-
-		this.setState(state);
-	};
-
-	addBasketToCart = (e, basket) => {
+	const addBasketToCart = (e, basket) => {
 		e.stopPropagation();
 
 		if (typeof window !== 'undefined') {
@@ -33,36 +33,33 @@ class BasketsContainer extends React.Component {
       cart.push(basket);
 
       window.localStorage.setItem('cart', JSON.stringify(cart));
-			this.eventEmitter.emit('editCart');
+			eventEmitter.emit('editCart');
 
-			this.setState({ basketForCart: basket }, this.toggleCartModal);
+			setBasketForCart(basket);
+			toggleCartModal();
     }
-	}
+	};
 
-	handleBasketButtonClick = (basketIndex) => {
+	const handleBasketButtonClick = (basketIndex) => {
 		if (typeof window !== 'undefined') {
 			if (basketIndex === 3) {
-				window.location.assign('/custom-basket');
+				window.location.assign(`/${locale}/custom-basket`);
 			} else {
-				window.location.assign(`/baskets?type=${basketsInfos[basketIndex].type}`);
+				window.location.assign(`/${locale}/baskets?type=${basketsInfos[basketIndex].type}`);
 			}
 		}
 	};
 
-	render() {
-		const { basketForCart, showCartModal } = this.state
-
-		return (
-			<Baskets
-				basketForCart={basketForCart}
-				addBasketToCart={this.addBasketToCart}
-				handleBasketButtonClick={this.handleBasketButtonClick}
-				showCartModal={showCartModal}
-				toggleCartModal={this.toggleCartModal}
-				baskets={[...basketsInfos, customBasketDetails]}
-			/>
-		)
-	}
+	return (
+		<Baskets
+			basketForCart={basketForCart}
+			addBasketToCart={addBasketToCart}
+			handleBasketButtonClick={handleBasketButtonClick}
+			showCartModal={showCartModal}
+			toggleCartModal={toggleCartModal}
+			baskets={[...basketsInfos, customBasketDetails]}
+		/>
+	);
 }
 
 export default BasketsContainer
