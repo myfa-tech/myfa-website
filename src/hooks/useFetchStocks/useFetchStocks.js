1ec30f36a9eb7cd9004adc83c23d8ad5442004e4
Florian Adonis <tanohzana@gmail.com>
2020-05-05 18:21:42 +0000

    fetch stocks from backend

--- src/hooks/useFetchStocks/useFetchStocks.js
+++ src/hooks/useFetchStocks/useFetchStocks.js
@@ -8,17 +8,9 @@ const useFetchStocks = (init = []) => {
 
   useEffect(() => {
     const fetchData = async () => {
-      // const fetchedStocks = await fetchStocks();
+      const fetchedStocks = await fetchStocks();
 
-      // setStocks(fetchedStocks);
-      setStocks([
-        { label: 'carots', need: 2, have: 3 },
-        { label: 'cucumber', need: 20, have: 3 },
-        { label: 'cucumber', need: 9, have: 2 },
-        { label: 'cucucscber', need: 18, have: 20 },
-        { label: 'cucewmber', need: 5, have: 3 },
-        { label: 'pomme de terre', need: 15, have: 3 },
-      ]);
+      setStocks(fetchedStocks);
     };
 
     fetchData();
