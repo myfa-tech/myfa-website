a56f088dd4e412bc6b58eb85d6192fb4700a0d14
Florian Adonis <tanohzana@gmail.com>
2020-05-07 17:52:20 +0000

    display articles fetched from Contentful
    WIP
    
    display first article
    
    publish second article
    
    display contentful articles on home page
    
    display article cover
    
    display small left images
    
    pull blog articles out of directory
    
    fix rerendering and articles fetching issue
    
    replace router dom routing by react reach

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,17 +1,13 @@
 
-// exports.onCreateWebpackConfig = ({
-//   stage,
-//   rules,
-//   loaders,
-//   plugins,
-//   actions,
-// }) => {
-//   actions.setWebpackConfig({
-//     node: {
-//       net: 'empty',
-//       tls: 'empty',
-//       dns: 'empty',
-//       fs:  'empty',
-//     },
-//   });
-// };
+exports.onCreatePage = async ({ page, actions }) => {
+  const { createPage } = actions;
+
+  // page.matchPath is a special key that's used for matching pages
+  // only on the client.
+  if (page.path.match(/^\/articles/)) {
+    page.matchPath = "/articles/*"
+
+    // Update the page.
+    createPage(page);
+  }
+};
