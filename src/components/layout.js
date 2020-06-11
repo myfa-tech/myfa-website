6fdb8a8125b14baed772b2a9944c8a7f10678c85
Florian Adonis <tanohzana@gmail.com>
2020-06-11 17:56:18 +0000

    make footer lazy loaded

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,11 @@
 
-import React from "react"
-import PropTypes from "prop-types"
+import React, { lazy, Suspense } from 'react';
+import PropTypes from 'prop-types';
 
-import Header from "./Header"
-import Footer from "./Footer"
+import Header from './Header';
+const Footer = lazy(() => import('./Footer'));
 
-import './layout.scss'
+import './layout.scss';
 
 const Layout = ({ children, className, hideHeader, noBackgroundColor, showLoginSignupModal, toggleShowLoginSignupModal }) => {
   return (
@@ -20,7 +20,9 @@ const Layout = ({ children, className, hideHeader, noBackgroundColor, showLoginS
       <div>
         <main>{children}</main>
       </div>
-      <Footer noBackgroundColor={noBackgroundColor} />
+      <Suspense fallback={'LOADING'}>
+        <Footer noBackgroundColor={noBackgroundColor} />
+      </Suspense>
     </div>
   )
 }
