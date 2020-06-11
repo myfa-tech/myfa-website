b58a8504d9a57dc15baa75c4d17e91a00656ab26
Florian Adonis <tanohzana@gmail.com>
2020-06-11 11:17:25 +0000

    add no jobs message

---
+++ src/pages/jobs/NoJobs/NoJobs.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './NoJobs.scss';
+
+const NoJobs = () => {
+  return (
+    <div id='no-jobs'>
+      <h2>Merci pour l'intérêt que vous portez à MYFA. Nous n'avons pas d'offre actuellement.</h2>
+      <p>Nous vous conseillons de regarder du côté d'entreprises où vous vous épanouirez presque aussi bien que chez nous : Facebok, Goggle, Twitler...</p>
+      <p>Bonne recherche</p>
+    </div>
+  );
+};
+
+export default NoJobs;
