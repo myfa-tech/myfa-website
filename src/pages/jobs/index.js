0936ed5e3a00a35764210367be21d6d0a15847a9
Florian Adonis <tanohzana@gmail.com>
2020-06-11 14:51:54 +0000

    Chore: add no jobs message (#64)

--- src/pages/jobs/index.js
+++ src/pages/jobs/index.js
@@ -5,13 +5,12 @@ import { Divider } from '@material-ui/core';
 
 import SEO from '../../components/seo';
 import Layout from '../../components/layout';
+import NoJobs from './NoJobs';
 
 import useTranslate from '../../hooks/useTranslate';
 import logoHandsSrc from '../../images/logo-hands.png';
 
 import './jobs.scss';
-
-const REACT_APP_BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 
 const JobsPage = () => {
   const [t] = useTranslate();
@@ -29,7 +28,7 @@ const JobsPage = () => {
   };
 
   const jobs = [
-    { title: 'Stage business developer', date: getDate(new Date('2020-05-26')), location: 'Paris, Station F / Remote', file: 'stage_myfa_bizdev.pdf' },
+    // { title: 'Stage business developer', date: new Date('2020-05-26'), location: 'Paris, Station F / Remote', file: 'stage_myfa_bizdev.pdf' },
   ];
 
   return (
@@ -45,21 +44,21 @@ const JobsPage = () => {
 
         <Divider />
 
-        {jobs.map(job => (
+        {jobs.length ? jobs.map(job => (
           <Row className='job-row'>
             <Col sm={1} className='img-container d-none d-sm-block'>
               <img src={logoHandsSrc} />
             </Col>
             <Col xs={10}>
               <h4>{job.title}</h4>
-              <div className='sub-info'>{job.date}</div>
+              <div className='sub-info'>{getDate(job.date)}</div>
               <div className='sub-info'><FaMapPin /> {job.location}</div>
             </Col>
             <Col sm={1} xs={2} className='buttons-container'>
               <Button className='download-btn' as='a' href={`/${job.file}`} target="_blank"><FaEye /></Button>
             </Col>
           </Row>
-        ))}
+        )) : <NoJobs />}
       </div>
     </Layout>
   );
