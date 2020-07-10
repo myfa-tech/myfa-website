f332abbbd6457bba01c9071e2b798ac64b8c2d78
Florian Adonis <tanohzana@gmail.com>
2020-07-10 18:38:24 +0000

    add baskets images

--- src/assets/basketsImgs.js
+++ src/assets/basketsImgs.js
@@ -1,17 +1,23 @@
 
 import fruitsSrc from '../images/fruits.jpg';
 import beautySrc from '../images/beauty.jpg';
+import chocolateBasketSrc from '../images/chocolate.jpg';
 import ramadanSugarSrc from '../images/ramadan-sugar.jpg';
 import ramadanFruitsSrc from '../images/ramadan-fruits.jpg';
 import ramadanFullSrc from '../images/ramadan-full.jpg';
 import morningPackSrc from '../images/morning.jpg';
 import veggiesPackSrc from '../images/veggies.jpg';
+import localRicePackSrc from '../images/local_rice.jpg';
+import riceAttiekePackSrc from '../images/rice_attieke.jpg';
 
 const basketsImgs = {
 	fruits: fruitsSrc,
 	beauty: beautySrc,
+	chocolate: chocolateBasketSrc,
 	morning: morningPackSrc,
 	veggies: veggiesPackSrc,
+	local_rice: localRicePackSrc,
+	rice_attieke: riceAttiekePackSrc,
 	ramadan_fruits: ramadanFruitsSrc,
 	ramadan_sugar: ramadanSugarSrc,
 	ramadan_full: ramadanFullSrc,
