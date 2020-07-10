f4d221fcc7cd9b703207c547f78d9852b4b5ae80
Florian Adonis <tanohzana@gmail.com>
2020-07-10 10:53:05 +0000

    add morning and veggies packs images and change translations

--- src/assets/basketsImgs.js
+++ src/assets/basketsImgs.js
@@ -4,10 +4,14 @@ import beautySrc from '../images/beauty.jpg';
 import ramadanSugarSrc from '../images/ramadan-sugar.jpg';
 import ramadanFruitsSrc from '../images/ramadan-fruits.jpg';
 import ramadanFullSrc from '../images/ramadan-full.jpg';
+import morningPackSrc from '../images/morning.jpg';
+import veggiesPackSrc from '../images/veggies.jpg';
 
 const basketsImgs = {
 	fruits: fruitsSrc,
 	beauty: beautySrc,
+	morning: morningPackSrc,
+	veggies: veggiesPackSrc,
 	ramadan_fruits: ramadanFruitsSrc,
 	ramadan_sugar: ramadanSugarSrc,
 	ramadan_full: ramadanFullSrc,
