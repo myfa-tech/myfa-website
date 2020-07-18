983821789677ca8411b09984c4a174c40106c9a5
Florian Adonis <tanohzana@gmail.com>
2020-07-18 12:17:44 +0000

    WIP - details products images

--- src/assets/productsImgs.js
+++ src/assets/productsImgs.js
@@ -20,29 +20,59 @@ import saltSrc from '../images/salt.png';
 import tomatoSrc from '../images/tomato.png';
 import whiteOnionSrc from '../images/white-onion.png';
 import yamSrc from '../images/yam.png';
+import avocadosSrc from '../images/avocado.png';
+import pineappleSrc from '../images/pineapple.jpg';
+import appleSrc from '../images/apple.jpg';
+import redCabbageSrc from '../images/red-cabbage.jpg';
+import hotChiliSrc from '../images/hot-chili.jpg';
+import margarineSrc from '../images/margarine.jpg';
+import tomatoPureeSrc from '../images/tomato-puree.jpg';
+import cookingOilSrc from '../images/cooking-oil.jpg';
+import blackPepperSrc from '../images/black-pepper.jpg';
+import localRice10Src from '../images/local_rice_10.jpg';
+import localRice25Src from '../images/local_rice_25.jpg';
+import sugarSrc from '../images/sugar.jpg';
+import coffeeSrc from '../images/coffee.jpg';
+import bonnetRougeSrc from '../images/bonnet-rouge.jpg';
+import juiceSrc from '../images/juice.jpg';
 
 const productsImages = {
-	bananas: bananaSrc,
-	coconut: coconutSrc,
-	mangoes: mangoSrc,
-	oranges: orangeSrc,
-	plantain: plantainSrc,
-	attieke: attiekeSrc,
-	potatoes: potatoSrc,
-	onions: whiteOnionSrc,
-	carots: carrotSrc,
-	yam: yamSrc,
-	french_beans: beansSrc,
-	palm_oil: palmOilSrc,
-	gombos: gomboSrc,
-	maggie: maggieSrc,
-	garlic: garlicSrc,
-	tomatoes: tomatoSrc,
-	lemon: citronSrc,
-	peanut_paste: peanutPasteSrc,
-	salt: saltSrc,
-	pepper_powder: pepperSrc,
-	ginger_powder: gingerSrc,
+	'bananas': bananaSrc,
+	'coconut': coconutSrc,
+	'mangoes': mangoSrc,
+	'bonnet_rouge': bonnetRougeSrc,
+	'oranges': orangeSrc,
+	'plantain': plantainSrc,
+	'attieke': attiekeSrc,
+	'juice': juiceSrc,
+	'potatoes': potatoSrc,
+	'onions': whiteOnionSrc,
+	'margarine': margarineSrc,
+	'sugar': sugarSrc,
+	'tomato_puree': tomatoPureeSrc,
+	'local_rice_10': localRice10Src,
+	'local_rice_25': localRice25Src,
+	'oil': cookingOilSrc,
+	'coffee': coffeeSrc,
+	'black_pepper': blackPepperSrc,
+	'carots': carrotSrc,
+	'red_cabbage': redCabbageSrc,
+	'1_apple': appleSrc,
+	'yam': yamSrc,
+	'avocados': avocadosSrc,
+	'pineapple': pineappleSrc,
+	'hot_chili': hotChiliSrc,
+	'french_beans': beansSrc,
+	'palm_oil': palmOilSrc,
+	'gombos': gomboSrc,
+	'maggie': maggieSrc,
+	'garlic': garlicSrc,
+	'tomatoes': tomatoSrc,
+	'lemon': citronSrc,
+	'peanut_paste': peanutPasteSrc,
+	'salt': saltSrc,
+	'pepper_powder': pepperSrc,
+	'ginger_powder': gingerSrc,
 };
 
 export default productsImages;
