754b76cc3b4a5e1176f4c4209fa9d0460bebccf8
Florian Adonis <tanohzana@gmail.com>
2020-08-15 11:05:01 +0000

    create sliders on home page for mobile devices

--- src/pages/home/Packs/Packs.js
+++ src/pages/home/Packs/Packs.js
@@ -2,6 +2,7 @@ import React, { lazy, Suspense, useState } from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { FaShoppingCart } from 'react-icons/fa';
+import { Carousel } from 'react-responsive-carousel';
 
 import LoadingItem from '../../../components/LoadingItem';
 const CartModal = lazy(() => import('../../../components/CartModal'));
@@ -52,7 +53,7 @@ const Packs = () => {
 				secondary={{ text: t('home_page.packs.packs_secondary'), link: '/packs' }}
 			/>
 
-			<Row className='articles-container justify-content-center'>
+			<Row className='articles-container justify-content-center desktop-display'>
 				{baskets.length ? baskets.map((basket) => (
 					<Col md={3} key={basket.type} onClick={() => handleBasketButtonClick(basket.type)}>
 						<div className='article-container'>
@@ -82,6 +83,40 @@ const Packs = () => {
 					</Col>
 				))}
 			</Row>
+
+			<div className='mobile-display mobile-slider-container'>
+				<Carousel centerMode centerSlidePercentage={100} className='custom-slider'>
+					{baskets.length ? baskets.map((basket) => (
+						<Col md={3} key={basket.type} onClick={() => handleBasketButtonClick(basket.type)}>
+							<div className='article-container'>
+								<div className='article-inner-container'>
+									<h4>{t(basket.labelTranslate)}</h4>
+									<h5>{t(basket.homeDescTranslate)}</h5>
+									<img src={basket.img} alt={basket.imgAlt} />
+								</div>
+								<Row className='price-and-buy-container'>
+									<Col xs={7} className='price-container'>
+										<p className='new-price-euro'>{basket.price}€</p>
+										<p className='new-price-cfa'>{basket.priceCFA} FCFA</p>
+									</Col>
+
+									<Col xs={5} className='cart-container'>
+										<div className='cart-button' onClick={(e) => addBasketToCart(e, basket)}>
+											<FaShoppingCart className='cart-icon' />
+										</div>
+									</Col>
+								</Row>
+							</div>
+						</Col>
+					)) :
+					[1, 2, 3].map((it, index) => (
+						<Col md={4} key={index}>
+							<LoadingItem />
+						</Col>
+					))}
+				</Carousel>
+			</div>
+
 			{showCartModal &&
 				<Suspense fallback={<SectionLoader />}>
 					<CartModal
